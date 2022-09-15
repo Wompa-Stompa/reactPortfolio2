2e1ffb50697484db227a4c44aa4190b0adea05e6
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-09-14 21:21:08 -0500

    initial

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from './components/about';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">
+                Portfolio
+            </a>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+    )
+
+
+}
